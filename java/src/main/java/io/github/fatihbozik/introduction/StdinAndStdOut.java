38ac00bf3818fe6422ee0b19980fc9d0a58fd31c
Fatih Bozik <bozikfatih@gmail.com>
2019-11-23 01:22:06 +0300

    Add Java Stdin and Stdout I Solution

---
+++ java/src/main/java/io/github/fatihbozik/introduction/StdinAndStdOut.java
@@ -0,0 +1,17 @@
+package io.github.fatihbozik.introduction;
+
+import java.util.Scanner;
+
+public class StdinAndStdOut {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int firstNumber = scanner.nextInt();
+        final int secondNumber = scanner.nextInt();
+        final int thirdNumber = scanner.nextInt();
+
+        System.out.println("\nOutput:");
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
+    }
+}
---
+++ java/src/test/java/io/github/fatihbozik/introduction/StdinAndStdOutTest.java
@@ -0,0 +1,27 @@
+package io.github.fatihbozik.introduction;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+
+/**
+ * @author Fatih Bozik
+ */
+public class StdinAndStdOutTest {
+    @Rule
+    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
+    public void shouldPrintTextsFromStdIn() {
+        systemInMock.provideLines("42", "177", "56");
+        StdinAndStdOut.main(new String[0]);
+        Assert.assertEquals("\nOutput:\n42\n177\n56\n", systemOutRule.getLog());
+    }
+}
